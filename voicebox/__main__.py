7500a2d18a2a66e0c42cdedf6f1c51c332d7b35f
Austin Bowen <austin.bowen.314@gmail.com>
2023-10-09 04:03:21 -0400

    Add language arg

--- voicebox/__main__.py
+++ voicebox/__main__.py
@@ -2,15 +2,18 @@ import argparse
 
 from voicebox.effects.dc_offset import RemoveDcOffset
 from voicebox.effects.normalize import Normalize
+from voicebox.tts.picotts import PicoTTS
+from voicebox.tts.tts import TTS
 from voicebox.voicebox import Voicebox
 
 
 def main():
     args = _parse_args()
 
+    tts = _get_tts(args)
     effects = _get_effects(args)
 
-    voicebox = Voicebox(effects=effects)
+    voicebox = Voicebox(tts, effects)
     voicebox.speak(args.text)
 
 
@@ -19,11 +22,19 @@ def _parse_args():
 
     parser.add_argument('text', help='Text to speak')
 
+    # TTS args
+    parser.add_argument('--lang', default='en-US', help='Language code')
+
+    # Effect args
     parser.add_argument('--scale-rate', type=float, help='Scale sample rate by this factor')
     parser.add_argument('--phaser', action='store_true', help='Add phaser effect')
     parser.add_argument('--glitch', action='store_true', help='Add glitch effect')
 
     return parser.parse_args()
+
+
+def _get_tts(args) -> TTS:
+    return PicoTTS(language=args.lang)
 
 
 def _get_effects(args):
