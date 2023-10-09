ced2bbff3e14767dc4b7d10971e5555b6c19ee12
Austin Bowen <austin.bowen.314@gmail.com>
2023-10-09 02:01:51 -0400

    Make get_audio_from_wav_file adjust to sample width

--- voicebox/tts/utils.py
+++ voicebox/tts/utils.py
@@ -6,16 +6,21 @@ import numpy as np
 from voicebox.types import Audio
 
 
-PCM_16_BIT_MAX_VALUE = 2 ** 15 - 1
-
-
 def get_audio_from_wav_file(file_path: Path) -> Audio:
     with wave.open(str(file_path), 'rb') as wav_file:
+        bits_per_sample = 8 * wav_file.getsampwidth()
         signal_bytes = wav_file.readframes(-1)
         sample_rate = wav_file.getframerate()
 
-    # Assuming 16-bit PCM
-    signal = np.frombuffer(signal_bytes, dtype=np.int16)
-    signal = signal.astype(float) / PCM_16_BIT_MAX_VALUE
+    dtype = {
+        8: np.uint8,
+        16: np.int16,
+        32: np.int32,
+    }[bits_per_sample]
+    signal = np.frombuffer(signal_bytes, dtype=dtype)
+
+    # Scale to [-1, 1]
+    max_value = 2 ** (bits_per_sample - 1)
+    signal = signal.astype(float) / max_value
 
     return Audio(signal, sample_rate)
