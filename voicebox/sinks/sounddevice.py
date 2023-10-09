40762bebfb930b4e43307318a6b0f6144d74fba0
Austin Bowen <austin.bowen.314@gmail.com>
2023-10-09 00:49:05 -0400

    Add SoundDevice sink

---
+++ voicebox/sinks/sounddevice.py
@@ -0,0 +1,12 @@
+import sounddevice as sd
+
+from voicebox.sinks.sink import Sink
+from voicebox.types import Audio
+
+
+class SoundDevice(Sink):
+    """An audio sink based on the [sounddevice](https://python-sounddevice.readthedocs.io/en/0.4.6/) library."""
+
+    def play(self, audio: Audio) -> None:
+        sd.play(audio.signal, audio.sample_rate)
+        sd.wait()
