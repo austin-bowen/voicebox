e1063056375d93f2cd8e22946b0e6364048c486b
Austin Bowen <austin.bowen.314@gmail.com>
2023-10-09 02:25:36 -0400

    Add ClipDistortion and TanhDistortion

---
+++ voicebox/effects/distortion.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+
+import numpy as np
+
+from voicebox.audio import Audio
+from voicebox.effects.effect import Effect
+
+
+@dataclass
+class ClipDistortion(Effect):
+    gain: float = 1.
+    max_value: float = 1.
+
+    def apply(self, audio: Audio) -> Audio:
+        audio.signal = np.clip(self.gain * audio.signal, -self.max_value, self.max_value)
+        return audio
+
+
+@dataclass
+class TanhDistortion(Effect):
+    gain: float = 1.
+
+    def apply(self, audio: Audio) -> Audio:
+        audio.signal = np.tanh(self.gain * audio.signal)
+        return audio
