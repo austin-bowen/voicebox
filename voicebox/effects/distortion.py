8ad54037118cd7afe2fd366e6c3345015061df70
Austin Bowen <austin.bowen.314@gmail.com>
2023-10-09 22:26:45 -0400

    Add Bitcrusher and Subsample effects

--- voicebox/effects/distortion.py
+++ voicebox/effects/distortion.py
@@ -7,6 +7,25 @@ from voicebox.effects.effect import Effect
 
 
 @dataclass
+class Bitcrusher(Effect):
+    bits: int = 4
+
+    def apply(self, audio: Audio) -> Audio:
+        min_sample = np.min(audio.signal)
+        max_sample = np.max(audio.signal)
+
+        # Scale to [0, 1]
+        signal = (audio.signal - min_sample) / (max_sample - min_sample)
+
+        values = 2 ** self.bits - 1
+        signal = np.round(values * signal) / values
+
+        audio.signal = signal * (max_sample - min_sample) + min_sample
+
+        return audio
+
+
+@dataclass
 class ClipDistortion(Effect):
     gain: float = 1.
     max_value: float = 1.
--- voicebox/effects/samplerate.py
+++ voicebox/effects/samplerate.py
@@ -12,3 +12,13 @@ class ChangeSampleRate(Effect):
     def apply(self, audio: Audio) -> Audio:
         audio.sample_rate = self.modifier(audio.sample_rate)
         return audio
+
+
+@dataclass
+class Subsample(Effect):
+    divisor: int = 2
+
+    def apply(self, audio: Audio) -> Audio:
+        audio.signal = audio.signal[::self.divisor]
+        audio.sample_rate //= self.divisor
+        return audio
