4dd929a04c12c00a5b81fee9af53b22578192316
Austin Bowen <austin.bowen.314@gmail.com>
2023-10-18 23:30:31 -0400

    Allow appending to existing wav files

--- voicebox/sinks/wavefile.py
+++ voicebox/sinks/wavefile.py
@@ -1,3 +1,4 @@
+import os
 import wave
 from dataclasses import dataclass
 from pathlib import Path
@@ -6,21 +7,47 @@ import numpy as np
 
 from voicebox.audio import Audio
 from voicebox.sinks.sink import Sink
+from voicebox.tts.utils import get_audio_from_wav_file
 from voicebox.types import FileOrPath
 
 
 @dataclass
 class WaveFile(Sink):
     file: FileOrPath
+    append: bool = False
     sample_width: int = 2
 
     def play(self, audio: Audio) -> None:
-        write_audio_to_wave(audio, self.file, sample_width=self.sample_width)
+        write_audio_to_wave(audio, self.file, append=self.append, sample_width=self.sample_width)
 
 
-def write_audio_to_wave(audio, file_or_path: FileOrPath, sample_width: int = 2) -> None:
-    if isinstance(file_or_path, Path):
+def write_audio_to_wave(
+        audio,
+        file_or_path: FileOrPath,
+        append: bool = False,
+        sample_width: int = 2,
+) -> None:
+    if isinstance(file_or_path, (Path, str)):
         file_or_path = str(file_or_path)
+        needs_append = append and os.path.isfile(file_or_path)
+    else:
+        # file_or_path is file-like object
+        needs_append = False
+
+    if needs_append:
+        existing_audio = get_audio_from_wav_file(file_or_path)
+
+        if audio.sample_rate != existing_audio.sample_rate:
+            raise ValueError(
+                f'Cannot append audio to existing file {file_or_path}: '
+                f'Sample rates do not match: '
+                f'new={audio.sample_rate}; '
+                f'existing={existing_audio.sample_rate}'
+            )
+
+        signal = np.concatenate([existing_audio.signal, audio.signal])
+    else:
+        signal = audio.signal
 
     bits_per_sample = 8 * sample_width
     dtype = {
@@ -31,7 +58,7 @@ def write_audio_to_wave(audio, file_or_path: FileOrPath, sample_width: int = 2) 
 
     # Assuming signal is in range[-1, 1], scale to [-max_value, max_value]
     max_value = 2 ** (bits_per_sample - 1) - 1
-    signal = np.round(audio.signal * max_value).astype(dtype)
+    signal = np.round(signal * max_value).astype(dtype)
     signal_bytes = signal.tobytes()
 
     with wave.open(file_or_path, 'wb') as wav_file:
