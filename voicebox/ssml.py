befe2bf2820f7fad18991930658df93a5dad0429
Austin Bowen <austin.bowen.314@gmail.com>
2023-11-05 19:09:36 -0500

    Add SSML.auto()

--- voicebox/ssml.py
+++ voicebox/ssml.py
@@ -6,6 +6,16 @@ from typing import List, Union, Dict
 class SSML(str):
     """A [Speech Synthesis Markup Language (SSML)](https://www.w3.org/TR/speech-synthesis/) string."""
 
+    @classmethod
+    def auto(cls, text: str) -> Union['SSML', str]:
+        """
+        Returns the ``text`` as SSML if it starts with ``'<speak>'``,
+        otherwise returns the ``text`` unaltered.
+        """
+
+        is_ssml = text.lstrip().startswith('<speak>')
+        return cls(text) if is_ssml else text
+
 
 class _SSMLElement:
     tag: str
