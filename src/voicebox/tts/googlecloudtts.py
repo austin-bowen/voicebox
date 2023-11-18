baa4814d8d27efaecb3083c886cc14bbacdf4a0b
Austin Bowen <austin.bowen.314@gmail.com>
2023-11-17 22:02:19 -0500

    Simplify googlecloudtts.py

--- src/voicebox/tts/googlecloudtts.py
+++ src/voicebox/tts/googlecloudtts.py
@@ -1,8 +1,6 @@
-import wave
 from dataclasses import dataclass, field
 from io import BytesIO
 
-import numpy as np
 from google.api_core import gapic_v1
 from google.cloud.texttospeech import (
     AudioConfig,
@@ -13,9 +11,10 @@ from google.cloud.texttospeech import (
 )
 
 from voicebox.audio import Audio
-from voicebox.tts.tts import TTS
-from voicebox.types import StrOrSSML
 from voicebox.ssml import SSML
+from voicebox.tts.tts import TTS
+from voicebox.tts.utils import get_audio_from_wav_file
+from voicebox.types import StrOrSSML
 
 
 @dataclass
@@ -36,24 +35,15 @@ class GoogleCloudTTS(TTS):
     def get_speech(self, text: StrOrSSML) -> Audio:
         self.audio_config.audio_encoding = AudioEncoding.LINEAR16
 
+        input_ = (SynthesisInput(ssml=text) if isinstance(text, SSML) else
+                  SynthesisInput(text=text))
+
         response = self.client.synthesize_speech(
-            input=SynthesisInput(ssml=text) if isinstance(text, SSML) else SynthesisInput(text=text),
+            input=input_,
             voice=self.voice_params,
             audio_config=self.audio_config,
             timeout=self.timeout,
         )
 
-        with BytesIO(response.audio_content) as wav_data:
-            with wave.open(wav_data, 'rb') as wav_file:
-                signal_bytes = wav_file.readframes(-1)
-                sample_rate = wav_file.getframerate()
-
-        signal = np.frombuffer(signal_bytes, dtype=np.int16)
-
-        # Scale to [-1, 1]
-        bits_per_sample = 16
-        max_value = 2 ** (bits_per_sample - 1) - 1
-        signal = signal.astype(float) / max_value
-        signal = signal.astype(np.float32)
-
-        return Audio(signal, sample_rate)
+        with BytesIO(response.audio_content) as wav_file:
+            return get_audio_from_wav_file(wav_file)
