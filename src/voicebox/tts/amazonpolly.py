3f6babd1a96665a1fc9b2dd06738fe97b5fdee80
Austin Bowen <austin.bowen.314@gmail.com>
2023-11-18 06:58:28 -0500

    Simplify amazonpolly.py

--- src/voicebox/tts/amazonpolly.py
+++ src/voicebox/tts/amazonpolly.py
@@ -9,7 +9,7 @@ from mypy_boto3_polly.literals import LanguageCodeType, EngineType, VoiceIdType
 from voicebox.audio import Audio
 from voicebox.ssml import SSML
 from voicebox.tts.tts import TTS
-from voicebox.tts.utils import add_optional_items
+from voicebox.tts.utils import add_optional_items, get_audio_from_samples
 from voicebox.types import StrOrSSML
 
 
@@ -22,13 +22,13 @@ class AmazonPolly(TTS):
     https://docs.aws.amazon.com/polly/latest/dg/API_SynthesizeSpeech.html
     """
 
-    polly_client: PollyClient
+    client: PollyClient
     """
     Boto3 Polly client, created by e.g.
 
     ``
     session = boto3.Session(...)
-    polly_client = session.client('polly')
+    client = session.client('polly')
     ``
     """
 
@@ -67,17 +67,11 @@ class AmazonPolly(TTS):
             ('LexiconNames', self.lexicon_names),
         ])
 
-        response = self.polly_client.synthesize_speech(**kwargs)
+        response = self.client.synthesize_speech(**kwargs)
 
         with closing(response['AudioStream']) as audio_stream:
             signal_bytes = audio_stream.read()
 
-        signal = np.frombuffer(signal_bytes, dtype=np.int16)
+        samples = np.frombuffer(signal_bytes, dtype=np.int16)
 
-        # Scale to [-1, 1]
-        bits_per_sample = 16
-        max_value = 2 ** (bits_per_sample - 1)
-        signal = signal.astype(float) / max_value
-        signal = signal.astype(np.float32)
-
-        return Audio(signal, self.sample_rate)
+        return get_audio_from_samples(samples, self.sample_rate)
