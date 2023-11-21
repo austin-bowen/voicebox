5be0773ebe9c81703d2f937ee0ac8059ee428c0f
Austin Bowen <austin.bowen.314@gmail.com>
2023-11-21 01:00:55 -0500

    Add device and latency to SoundDevice. Default latency to 0.1s to improve playback stability.

--- src/voicebox/sinks/sounddevice.py
+++ src/voicebox/sinks/sounddevice.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Union, Literal
 
 import sounddevice as sd
 
@@ -6,12 +7,36 @@ from voicebox.sinks.sink import Sink
 from voicebox.audio import Audio
 
 
+Device = Union[int, str]
+Latency = Union[float, Literal['low', 'high']]
+
+
 @dataclass
 class SoundDevice(Sink):
     """An audio sink based on the [sounddevice](https://python-sounddevice.readthedocs.io/en/0.4.6/) library."""
 
+    device: Device = None
+    """
+    Device index or query string specifying the device to be used.
+    If ``None`` (default), a device will be selected automatically.
+    See function ``sounddevice.query_devices()`` for valid choices.
+    """
+
     blocking: bool = True
     """Whether to wait for playback to finish before returning."""
 
+    latency: Latency = 0.1
+    """
+    The desired latency in seconds. The special values 'low' and 'high'
+    select the device's default low and high latency, respectively.
+    Lower latency reduces time to playback; higher latency improves stability.
+    """
+
     def play(self, audio: Audio) -> None:
-        sd.play(audio.signal, audio.sample_rate, blocking=self.blocking)
+        sd.play(
+            audio.signal,
+            audio.sample_rate,
+            blocking=self.blocking,
+            device=self.device,
+            latency=self.latency,
+        )
--- tests/sinks/test_sounddevice.py
+++ tests/sinks/test_sounddevice.py
@@ -4,20 +4,31 @@ from unittest.mock import patch
 from parameterized import parameterized
 
 from tests.utils import build_audio
-from voicebox.sinks.sounddevice import SoundDevice
+from voicebox.sinks.sounddevice import SoundDevice, Device, Latency
 
 
 class SoundDeviceTest(unittest.TestCase):
     def test_constructor_defaults(self):
         sink = SoundDevice()
+        self.assertIsNone(sink.device)
         self.assertTrue(sink.blocking)
+        self.assertEqual(0.1, sink.latency)
 
-    @parameterized.expand([True, False])
+    @parameterized.expand([
+        (None, True, 0.1),
+        ('some-device', False, 'low'),
+    ])
     @patch('voicebox.sinks.sounddevice.sd')
-    def test_play(self, blocking: bool, mock_sd):
+    def test_play(
+            self,
+            device: Device,
+            blocking: bool,
+            latency: Latency,
+            mock_sd,
+    ):
         audio = build_audio()
 
-        sink = SoundDevice(blocking=blocking)
+        sink = SoundDevice(device, blocking, latency)
         sink.play(audio)
 
         mock_sd.play.assert_called_once()
@@ -27,5 +38,7 @@ class SoundDeviceTest(unittest.TestCase):
         self.assertIs(mock_call.args[0], audio.signal)
         self.assertEqual(audio.sample_rate, mock_call.args[1])
 
-        self.assertEqual(1, len(mock_call.kwargs))
+        self.assertEqual(3, len(mock_call.kwargs))
         self.assertEqual(blocking, mock_call.kwargs['blocking'])
+        self.assertEqual(device, mock_call.kwargs['device'])
+        self.assertEqual(latency, mock_call.kwargs['latency'])
