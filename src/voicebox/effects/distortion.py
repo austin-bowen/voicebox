6e66764735eb0f496e18b2861123cc1fe5986235
Austin Bowen <austin.bowen.314@gmail.com>
2023-12-18 22:40:35 -0500

    Remove ClipDistortion effect, as it is covered by pedalboard.Clipping

--- src/voicebox/effects/distortion.py
+++ src/voicebox/effects/distortion.py
@@ -5,17 +5,7 @@ import numpy as np
 from voicebox.audio import Audio
 from voicebox.effects.effect import Effect
 
-__all__ = ['ClipDistortion', 'TanhDistortion']
-
-
-@dataclass
-class ClipDistortion(Effect):
-    gain: float = 1.
-    max_value: float = 1.
-
-    def apply(self, audio: Audio) -> Audio:
-        audio.signal = np.clip(self.gain * audio.signal, -self.max_value, self.max_value)
-        return audio
+__all__ = ['TanhDistortion']
 
 
 @dataclass
