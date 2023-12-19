36ee46a51c1a15a96313fdabbb8f3f5fe350d28c
Austin Bowen <austin.bowen.314@gmail.com>
2023-12-18 22:33:48 -0500

    Add pedalboard_effects

--- src/voicebox/effects/pedalboard.py
+++ src/voicebox/effects/pedalboard.py
@@ -1,11 +1,16 @@
+__all__ = [
+    'PedalboardEffect',
+    'pedalboard_effects',
+]
+
 from dataclasses import dataclass
+from typing import Sequence
 
 import pedalboard
 
 from voicebox.audio import Audio
 from voicebox.effects.effect import Effect
 
-__all__ = ['PedalboardEffect']
 
 
 @dataclass
@@ -17,12 +22,12 @@ class PedalboardEffect(Effect):
     https://spotify.github.io/pedalboard/reference/pedalboard.html
 
     Example:
-    ::
-        from voicebox.effects import PedalboardEffect
-        import pedalboard
-        voicebox.effects.append(
-            PedalboardEffect(pedalboard.Reverb())
-        )
+        >>> from voicebox.effects import PedalboardEffect
+        >>> import pedalboard
+        >>> effects = [
+        >>>     ...,
+        >>>     PedalboardEffect(pedalboard.Reverb()),
+        >>> ]
     """
 
     plugin: pedalboard.Plugin
@@ -30,3 +35,21 @@ class PedalboardEffect(Effect):
     def apply(self, audio: Audio) -> Audio:
         audio.signal = self.plugin.process(audio.signal, audio.sample_rate, reset=True)
         return audio
+
+
+def pedalboard_effects(
+        *effects: pedalboard.Plugin,
+) -> Sequence[PedalboardEffect]:
+    """
+    Creates a sequence of PedalboardEffect objects from Pedalboard plugins.
+
+    Example:
+        >>> from voicebox.effects import pedalboard_effects
+        >>> import pedalboard
+        >>> effects = pedalboard_effects(
+        >>>     pedalboard.Distortion(),
+        >>>     pedalboard.Reverb(),
+        >>> )
+    """
+
+    return [PedalboardEffect(plugin) for plugin in effects]
