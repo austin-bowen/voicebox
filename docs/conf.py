c6f06c687ca17d5dc258b859190ffca1ba633b04
Austin Bowen <austin.bowen.314@gmail.com>
2023-12-19 20:43:35 -0500

    Include version in documentation

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,7 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+from voicebox import __version__
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -10,6 +11,9 @@
 project = 'voicebox'
 copyright = '2023 Austin Bowen'
 author = 'Austin Bowen'
+
+version = __version__
+release = __version__
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -22,8 +26,6 @@ extensions = [
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
